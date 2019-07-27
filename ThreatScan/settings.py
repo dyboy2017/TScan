82b2a126f97967a817ad237a625f3cf8ddf4e8e4
dyboy2017 <1099718640@qq.com>
2019-07-28 04:09:13 +0800

    线上测试第一版

--- ThreatScan/settings.py
+++ ThreatScan/settings.py
@@ -25,9 +25,9 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 SECRET_KEY = 'n60wsm40emq2u_^+h%pq8#(%g4gcwou_g$oows)+nacu3uz3gf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
